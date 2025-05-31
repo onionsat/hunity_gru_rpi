4337f2fda2a962bc6f99dd3bb9cf8f9f8252c55f
Mihályffy Róbert <148487611+Robeszpier4@users.noreply.github.com>
2025-05-31 20:13:07 +0200

    nemtudoknevetadniennek:(

---
+++ functions.py
@@ -0,0 +1,275 @@
+import json
+import time
+import mysql.connector
+from smbus2 import SMBus, i2c_msg
+import RPi.GPIO as GPIO
+import bme280
+
+# function to load credentials from json file
+def loadCredentials(filename):
+    success = False
+
+    while success == False:
+        try:
+            with open(filename, "r") as credentials_file:
+                try:
+                    credentials = json.load(credentials_file)
+                    success = True
+                except:
+                    print(f"Error loading credentials from {filename}! Retry in 1 second!")
+                    time.sleep(1)
+        except:
+            print(f"Unable to open {filename}! Retry in 1 second!")
+            time.sleep(1)
+
+    return credentials
+
+# function to initialize database connection
+def initDB(host, user, password, database):
+    success = False
+
+    while success == False:
+        try:
+            conn = mysql.connector.connect(host=host, user=user, password=password, database=database)
+            success = True
+        except:
+            print("Unable to connect to the database! Retry in 1 second!")
+            time.sleep(1)
+
+    cursor = conn.cursor()
+
+    return conn, cursor
+
+# function to initialize I2C bus
+def initI2C(bus_number):
+    success = False
+
+    while success == False:
+        try:
+            bus = SMBus(bus_number)
+            success = True
+        except:
+            print(f"Unable to initialize I2C bus {bus_number}! Retry in 1 second!")
+            time.sleep(1)
+
+    return bus
+
+# function to initialize GPIOs for enabling experiments
+def initEn(pins):
+    GPIO.setmode(GPIO.BCM) # uses gpio numbering
+
+    success = True
+    
+    while success == False:
+        try: 
+            # Setting all gpios to output in the pins list, intial value is LOW
+            for i in pins:
+                GPIO.setup(i, GPIO.OUT)
+                GPIO.set(GPIO.LOW)
+            
+            success = True
+        except:
+            print("Error settting up GPIOs! Retry in 1 second!")
+            time.sleep(1)
+
+# function to initialize BME280 sensor
+def initBME280():
+    while True:
+        try:
+            # Initialize BME280 sensor
+            bme_calibration = bme280.load_calibration_params(SMBus(1), 0x76)
+
+            return bme_calibration
+        except:
+            print("Error initializing BME280 sensor! Retry in 1 second!")
+            time.sleep(1)
+
+# function to initialize everything
+def fullInit(filename, pins, bus_number):
+    # loading credentials
+    credentials = loadCredentials(filename)
+
+    # initializing database connection
+    conn, cursor = initDB(credentials["db"]["host"], credentials["db"]["user"], credentials["db"]["password"], credentials["db"]["database"])
+
+    # initializing I2C bus
+    bus = initI2C(bus_number)
+
+    # initializing Enable GPIOs
+    initEn(pins)
+
+    # initializing BME280 sensor
+    bme_calibration = initBME280()
+
+    print("Initialization complete!")
+
+    return conn, cursor, bus, bme_calibration
+
+# function to switch experimentId on and off
+def switchExperiment(conn, cursor, experimentId, pins):
+    # try to get switch state from the database
+    try:
+        onOffQuery = "SELECT switch FROM switch_exp WHERE experimentid = %s"
+        cursor.execute(onOffQuery, (experimentId,))
+        switchState = cursor.fetchall()
+    except:
+        print(f"Error querying switch for experiment {experimentId}")
+        return # stop the function if there is an error
+    
+    try:
+        if switchState[0][0] == 1:
+            GPIO.output(pins[experimentId-1], GPIO.HIGH) # turn on the experiment
+        elif switchState[0][0] == 0:
+            GPIO.output(pins[experimentId-1], GPIO.LOW) # turn off the experiment
+    except:
+        print(f"Error switching experiment {experimentId}")
+
+# function to read from experimentId
+def readExperiment(conn, cursor, experimentId, i2cAddresses, bus):
+    successReading = True
+
+    try:
+        readCommand = i2c_msg.read(i2cAddresses[experimentId-1], 16)
+        bus.i2c_rdwr(readCommand)
+        experimentData = list(readCommand)
+
+        print(f"Succesfully read from experiment {experimentId} data: ")
+        for i in range(0, 16):
+            print(f"{experimentData[i]:02X}", end="")
+        print()
+    except:
+        print(f"Error reading from experiment {experimentId}")
+        successReading = False
+    
+    # trying to add the result of the successful read operation to the database
+    if successReading:
+        experimentDataString = ""
+        for i in range(0, 16):
+            experimentDataString = experimentDataString + f"{experimentData[i]:02x}"
+
+        # insert the data into the experimentdata table
+        try:    
+            cursor.execute("INSERT INTO experimentdata (experimentid, expdata, success, timestamp) VALUES (%s, %s, %s, current_timestamp())", (experimentId, experimentDataString, 1))
+            conn.commit()
+        except:
+            print("Error pushing successful read to the database")
+    # trying to add the result of the unsuccessful read operation to the database
+    else:
+        try:
+            # Insert unsuccessful read into the experiment_data table
+            cursor.execute("INSERT INTO experimentdata (experimentid, success, timestamp) VALUES (%s, %s, current_timestamp())", (experimentId, 0))
+            conn.commit()
+        except:
+            print("Error pushing unsuccessful read to the database")
+    
+    # if reading was unsuccessful, wait for 1 second
+    if not successReading:
+        time.sleep(1)
+
+def writeCommand(conn, cursor, bus, experimentId, i2cAddresses):
+    commandAvailbale = False
+
+    try:
+        # get command from writecommands table
+        queryWritecommand = "SELECT id, command FROM writecommands WHERE experimentid = %s and sent = 0 ORDER BY sent_to_master ASC LIMIT 1"
+        cursor.execute(queryWritecommand, (experimentId,))
+        writeCommand = cursor.fetchall()
+
+        # if there is new command, update the sent in the writecommands table to 2
+        if len(writeCommand) == 1:
+            cursor.execute("UPDATE writecommands SET sent = %s WHERE id = %s", (2, writeCommand[0][0]))
+            conn.commit()
+        # if there isn't a command stop the function
+        else:
+            print(f"No command available for experiment {experimentId}")
+            return
+    except:
+        print("Error querying command")
+        return
+    
+    sendingStatus = 2
+
+    # check if the command is valid
+    currentCommandString = writeCommand[0][1]
+    currentCommand = []
+    
+    try:
+        for i in range(0, 8):
+            currentCommand.append(int(currentCommandString[i*2:i*2+2], 16))
+    except:
+        sendingStatus = -1
+    
+    if sendingStatus == 2:
+        # try to send the command to the device
+        try:
+            writeCommandi2c = i2c_msg.write(i2cAddresses[experimentId-1], currentCommand)
+            bus.i2c_rdwr(writeCommandi2c)
+
+            print(f"Succesfully sent command to experiment {experimentId}: {currentCommandString}")
+            sendingStatus = 1
+        except:
+            print(f"Error sending command to experiment {experimentId}")            
+            sendingStatus = -1
+
+            time.sleep(1) # if sending was unsuccessful, wait for 1 second
+    
+    # update the sent in the writecommands table
+    try:
+        cursor.execute("UPDATE writecommands SET sent = %s, sent_to_experiment = current_timestamp() WHERE id = %s", (sendingStatus, writeCommand[0][0]))
+        conn.commit()
+    except:
+        print("Error updating writecommands table")
+
+# function to send timesync command
+def timesyncCommand(bus):
+    timestamp = int(time.time())
+    timestampCommandOnlytimestamp = list(timestamp.to_bytes(4, byteorder='little'))
+
+    timestampCommand = [0x54]
+
+    for i in range(0, 4):
+        timestampCommand.append(timestampCommandOnlytimestamp[i])
+
+    try:
+        timeSyncCommand = i2c_msg.write(0x00, timestampCommand)
+        bus.i2c_rdwr(timeSyncCommand)
+        print("Timesync command sent successfully")
+    except:
+        print("Error sending timesync command")
+
+def pushBMEdata(conn, cursor, bme_calibration, bus):
+    try:
+        bme280_data = bme280.sample(bus, 0x76, bme_calibration)
+        temperature = bme280_data.temperature
+        pressure = bme280_data.pressure
+        humidity = bme280_data.humidity
+    except:
+        print("Error reading BME280 sensor data")
+
+        return
+
+    print(f"temperature: {temperature} pressure: {pressure} humidity: {humidity}")
+
+    try:
+        insertBME280 = "INSERT INTO bmedata (temperature, pressure, humidity, timestamp) VALUES (%s, %s, %s, current_timestamp())"
+        cursor.execute(insertBME280, (temperature, pressure, humidity))
+        conn.commit()
+    except:
+        print("Error puting BME280 sensor data into database")
+
+# function to update code time
+def raspberryAlive(conn, cursor):
+    aliveSQL = "UPDATE raspberry_alive SET unixtimestamp = current_timestamp() WHERE id = 1"
+
+    try:
+        cursor.execute(aliveSQL)
+        conn.commit()
+    except:
+        print("Error updating raspberry_alive table")
+
+# function to reintialize i2c
+def reinitI2C(bus):
+    try:
+        bus.close()
+    except:
+        print("Error reinitializing I2C bus")
--- nmhhtester.py
+++ nmhhtester.py
@@ -1,294 +1,38 @@
-from smbus2 import SMBus, i2c_msg
-import mysql.connector
+import functions
 import time
-import json
-import RPi.GPIO as GPIO
-import bme280
-
-# function to load credentials from json file
-def loadCredentials(filename):
-    success = False
-
-    while success == False:
-        try:
-            with open(filename, "r") as credentials_file:
-                try:
-                    credentials = json.load(credentials_file)
-                    success = True
-                except:
-                    print(f"Error loading credentials from {filename}! Retry in 1 second!")
-                    time.sleep(1)
-        except:
-            print(f"Unable to open {filename}! Retry in 1 second!")
-            time.sleep(1)
-
-    return credentials
-
-# function to initialize database connection
-def initDB(host, user, password, database):
-    success = False
-
-    while success == False:
-        try:
-            conn = mysql.connector.connect(
-                host=host,
-                user=user,
-                password=password,
-                database=database
-            )
-            success = True
-        except:
-            print("Unable to connect to the database! Retry in 1 second!")
-            time.sleep(1)
-
-    cursor = conn.cursor()
-
-    return conn, cursor
-
-def initI2C(bus_number):
-    success = False
-
-    while success == False:
-        try:
-            bus = SMBus(bus_number)
-            success = True
-        except:
-            print(f"Unable to initialize I2C bus {bus_number}! Retry in 1 second!")
-            time.sleep(1)
-
-    return bus
-
-def initEn(pins):
-    GPIO.setmode(GPIO.BCM) # uses gpio numbering
-
-    success = True
-    
-    while success == False:
-        try: 
-            # Setting all gpios to output in the pins list
-            for i in pins:
-                GPIO.setup(i, GPIO.OUT)
-                GPIO.set(GPIO.LOW)
-            
-            success = True
-        except:
-            print("Error settting up GPIOs! Retry in 1 second!")
-            time.sleep(1)
-
-def initBME280():
-    while True:
-        try:
-            # Initialize BME280 sensor
-            bme_load = bme280.load_calibration_params(SMBus(1), 0x76)
-
-            return bme_load
-        except:
-            print("Error initializing BME280 sensor! Retry in 1 second!")
-            time.sleep(1)
 
 
-# intializing GPIOs
-gpio_pins = [13,6,5,11,9,10]
-initEn(gpio_pins)
-# getting credentials from json file
-credentials = loadCredentials("credentials_dev.json")
-# initializing database connection
-conn, cursor = initDB(credentials["db"]["host"], credentials["db"]["user"], credentials["db"]["password"], credentials["db"]["database"])
-# initializing I2C bus 1
-bus = initI2C(1)
-# initializing BME280 sensor
-bme280_calibration_params = initBME280()
-# start address
-address = 0x50
-# initlial state of gpios
-gpioState = [0, 0, 0, 0, 0, 0]
-# everything set up successfully
-print("Everything initialized successfully!")
+# intialization parameters
+i2cAddresses = [0x50, 0x51, 0x52, 0x53, 0x54, 0x55]
+pins = [13,6,5,11,9,10]
+filename = "credentials.json"
+bus_number = 1
+
+# full intialization
+conn, cursor, bus, bme_calibration = functions.fullInit(filename, pins, bus_number)
+
+experimentId = 1
 
 while True:
-    # switching mechanism
-    successSwitch = True
+    functions.switchExperiment(conn, cursor, experimentId, pins)
 
-    try:
-        onoffquery = "SELECT switch FROM switch_exp WHERE experimnetid = %s"
-        cursor.execute(onoffquery, (address - 0x50 + 1,))
+    functions.readExperiment(conn, cursor, experimentId, i2cAddresses, bus)
 
-        switch = cursor.fetchall()
-    except:
-        successSwitch = False
-        print(f"Error querying switch for experiment {address - 0x50 + 1}")
-    
-    if successSwitch:
-        try:
-            if switch[0][0] == 1 and gpioState[address - 0x50] == 0:
-                GPIO.output(gpio_pins[address - 0x50], GPIO.HIGH) # turn on the experiment
-            elif switch[0][0] == 0 and gpioState[address - 0x50] == 1:
-                GPIO.output(gpio_pins[address - 0x50], GPIO.LOW)
-        except:
-            print(f"Error switching experiment {address - 0x50 + 1}")
+    functions.writeCommand(conn, cursor, bus, experimentId, i2cAddresses)
 
-    # read operation
-    successReading = True
-
-    try:
-        readCommand = i2c_msg.read(address, 16)
-        bus.i2c_rdwr(readCommand)
-        experimentData = list(readCommand)
-        print(f"Succesfully read from experiment {address - 0x50 + 1} data: ")
-
-        for i in range(0, 16):
-            print(f"{experimentData[i]:02X}", end="")
-
-        print()
-    except:
-        print(f"Error reading from experiment {address - 0x50 + 1}")
-        successReading = False
-    
-    # trying to add the result of the read operation to the database
-    if successReading:
-        experimentDataString = ""
-
-        for i in range(0, 16):
-            experimentDataString = experimentDataString + f"{experimentData[i]:02x}"
-
-        try:
-            # snsert the data into the experimentdata table
-            cursor.execute("INSERT INTO experimentdata (experimentid, expdata, success, timestamp) VALUES (%s, %s, %s, current_timestamp())", (address - 0x50 + 1, experimentDataString, 1))
-            conn.commit()
-        except:
-            print("Error pushing successful read to the database")
-    else:
-        # reading failed
-        try:
-            # Insert the data into the experiment_data table
-            cursor.execute("INSERT INTO experimentdata (experimentid, success, timestamp) VALUES (%s, %s, current_timestamp())", (address - 0x50 + 1, 0))
-            conn.commit()
-        except:
-            print("Error pushing unsuccessful read to the database")
-    
-    # if reading was unsuccessful, wait for 1 second
-    if not successReading:
+    if experimentId < 6:
         time.sleep(1)
-    
-    # sending write command is there is any for current experiment
-    # checking for command
-    getNewCommandSuccess = True
-    try:
-        # get command from writecommands table
-        queryWritecommand = "SELECT id, command FROM writecommands WHERE experimentid = %s and sent = 0 ORDER BY sent_to_master ASC LIMIT 1"
-        cursor.execute(queryWritecommand, (address - 0x50 + 1,))
-        writeCommand = cursor.fetchall()
+        experimentId += 1
+    elif experimentId == 6:
+        functions.timesyncCommand(bus)
 
-        # if there is new command, update the sent in the writecommands table to 2
-        if len(writeCommand) == 1:
-            cursor.execute("UPDATE writecommands SET sent = %s WHERE id = %s", (2, writeCommand[0][0]))
-            conn.commit()
-    except:
-        print("Error query command")
-        getNewCommandSuccess = False
-
-    # if query was succesful
-    successWrite = True
-    if getNewCommandSuccess:
-        # if there is a command, send it
-        if len(writeCommand) == 1:
-            currentCommandString = writeCommand[0][1]
-            currentCommand = []
-
-            # make th command into a list of integers
-            vaildCommand = True
-            try:
-                for i in range(0, 8):
-                    currentCommand.append(int(currentCommandString[i*2:i*2+2], 16))
-            except:
-                vaildCommand = False
-                successWrite = False
-
-            # trying to send the command
-            if vaildCommand:
-                try:
-                    # send the command to the device
-                    writeCommandi2c = i2c_msg.write(address, currentCommand)
-                    bus.i2c_rdwr(writeCommandi2c)
-
-                    print(f"Succesfully sent command to experiment {address - 0x50 + 1}: {currentCommandString}")
-                except:
-                    print(f"Error sending command to experiment {address - 0x50 + 1}")
-                    successWrite = False
-
-            if successWrite == True:
-                # if sending succesful, update the sent in the writecommands table to -1
-                try:
-                    # update sent in the writecommands table, 1 -> send successfully
-                    cursor.execute("UPDATE writecommands SET sent = %s, sent_to_experiment = current_timestamp() WHERE id = %s", (1, writeCommand[0][0]))
-                    conn.commit()
-                except:
-                    print("Error update write successful")
-            else:
-                # if sending failed, update the sent in the writecommands table to -1
-                try:
-                    # update sent in the writecommands table, -1 -> failed to send
-                    cursor.execute("UPDATE writecommands SET sent = %s, sent_to_experiment = current_timestamp() WHERE id = %s", (-1, writeCommand[0][0]))
-                    conn.commit()
-                except:
-                    print("Error update write unsuccessful")
-            
-    if not successWrite:
-        # if write was unsuccessful, wait for 1 second
+        time.sleep(1)
+        functions.reinitI2C(bus)
         time.sleep(1)
 
-    # check if there is any experiment or new cycle
-    if address == 0x55:
-        # sending generall call timesync command
-        timestamp = int(time.time())
-        timestampCommand_onlytimestamp = list(timestamp.to_bytes(4, byteorder='little'))
-
-        timestampCommand = [0x54]
-
-        for i in range(0, 4):
-            timestampCommand.append(timestampCommand_onlytimestamp[i])
-
-        try:
-            timeSyncCommand = i2c_msg.write(0x00, timestampCommand)
-            bus.i2c_rdwr(timeSyncCommand)
-        except:
-            print(f"Timesync command were unsuccessful")
-        
-        time.sleep(2)
-
-        address = 0x50
-    else:
-        time.sleep(1)
-        address = address + 1
+        experimentId = 1
     
-    # reading BME280 sensor data
-    successBME280 = True
-    
-    try:
-        bme280_data = bme280.sample(SMBus(1), 0x76, bme280_calibration_params)
-    except:
-        successBME280 = False
-        print("Error reading BME280 sensor data")
-    
-    if successBME280:
-        print(f"temperature: {bme280_data.temperature} pressure: {bme280_data.pressure} humidity: {bme280_data.humidity}")
+    functions.pushBMEdata(conn, cursor, bme_calibration, bus)
 
-        try:
-            insertBME280 = "INSERT INTO bmedata (temperature, pressure, humidity, timestamp) VALUES (%s, %s, %s, current_timestamp())"
-
-            cursor.execute(insertBME280, (bme280_data.temperature, bme280_data.pressure, bme280_data.humidity))
-            conn.commit()
-        except:
-            print("Error puting BME280 sensor data into database")
-
-    # putting timestamp in raspberry_alive table
-    aliveSQL = "UPDATE raspberry_alive SET unixtimestamp = current_timestamp() WHERE id = 1"
-    aliveUpdateBool = True
-    try:
-        cursor.execute(aliveSQL)
-        conn.commit()
-    except:
-        aliveUpdateBool = False
-    
-    if not aliveUpdateBool:
-        print("Raspberry alive timestamp not updated successfully")
+    # keeps treck if the program is alive
+    functions.raspberryAlive(conn, cursor)
